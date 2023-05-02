9e8380beef4971410533fe9ae426c3095aa204ca
metrokotHome <maksimsmir94@gmail.com>
2023-05-03 02:41:41 +0300

    add line method

--- MinSquares.py
+++ MinSquares.py
@@ -13,15 +13,23 @@ def getNumber02(x, type):  # проверка на ввод числа
             except ValueError:
                 print('Введите запись типа: ' + str(type))
 def dotsInput():
-   x,y = [],[]
+   xy = []
    for i in range(getNumber02('количество точек','int')):
-       x,y = getNumber02(f'x_{i}','float'),getNumber02(f'y_{i}','float')
-   return x, y
+       xy.append((getNumber02(f'x_{i}','float'),getNumber02(f'y_{i}','float')))
+   return xy
 
 class MinSquares:
+    dictMS = {'x': [], 'y': []}
+    a, b = None, None # дописать
+    def func(self,x): return self.a*x+self.b
     def graphics(self):
         pass
-    def gaussMethod(self):
+    def lineMethod(self):
+        print(f"y = {(a:= ((y := sum(self.dictMS['y'])) - (n := len(self.dictMS['x'])) * (b := ((sum(list(map(lambda x, y: x * y, self.dictMS['x'], self.dictMS['y'])))) - ((x2 := sum(list(map(lambda x: x ** 2, self.dictMS['x'])))) * y / (x := sum(self.dictMS['x'])))) / ((x - (x2 * n / x))))) / x)}x + {b}")
+
+    def squareMethod(self):
         pass
     def __init__(self):
-        x,y = input()
+
+        self.dictMS['x'], self.dictMS['y'] = zip(*dotsInput())
+
