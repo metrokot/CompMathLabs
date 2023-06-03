1be8a07fc18662d736aeccf424a650d9d3955173
Max <128371845+metrokot@users.noreply.github.com>
2023-06-03 19:03:00 +0300

    Update UpLodka.py

--- UpLodka.py
+++ UpLodka.py
@@ -1,22 +1,29 @@
 import math
 
-def parasha(m,n):
-    p, h, t = int((3 * 26 + 1) / (26 + 2)), 1 / 5, 1 / 5
+
+def parasha(m, n):
+    p, h, t = (3 * 26 + 1) / (26 + 2), 1 / 5, 1 / 5
     u1, u2 = [[0.0] * (n + 1)] * (m + 1), [[0.0] * 2] * (m + 1)
     u2[m - 1][1] = 1
-    for i in range(m+1):
+    for i in range(m + 1):
         for j in range(n + 1):
-            if j==0 and i>0 and i<m: u1[i][j] = p*math.sin(math.pi*(i*h))
-            else: u1[i][j] = 0
-    for i in range(1,m):
-        for j in range(1,n + 1):
-            u1[i][j] = (u1[i-1][j-1] - 2*u1[i][j-1]+u1[i+1][j-1])*t/(h*h) + u1[i][j-1]
-            if (j*t)==0.2: u2[i][0],u2[i][1] = u2[i][j],i*h
+            if j == 0 and i > 0 and i < m: u1[i][j] = p * math.sin(math.pi * (i * h))
+            print(u1[i][j])
+    for i in range(1, m):
+        for j in range(1, n + 1):
+            u1[i][j] = ((u1[i - 1][j - 1] - 2.0 * u1[i][j - 1] + u1[i + 1][j - 1]) * t / (h * h) + u1[i][j - 1])
+            if (j * t) == 0.2: u2[i][0], u2[i][1] = u2[i][j], i * h
     for i in range(m):
         print(f'''
         {u2[i][0]:-f}\t{u2[i][1]:-f}
         тут не понимаю вывод 
     ''')
-parasha(5,80)
-parasha(10,80)
-parasha(10,250)
+    print(u1)
+
+
+parasha(5, 80)
+print('1')
+#parasha(10, 80)
+#print('2')
+#parasha(10, 250)
+
